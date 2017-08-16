0884ddc07653f3ee69aab14450282db2a1df19e8
TomasVotruba <tomas.vot@gmail.com>
2017-08-16 23:23:24 +0200

    decouple PhpLocAnalyzer

--- src/Analyzer/PhpLocAnalyzer.php
+++ src/Analyzer/PhpLocAnalyzer.php
@@ -1,7 +1,69 @@
 <?php declare(strict_types=1);
 
+namespace TomasVotruba\ShopsysAnalysis\Analyzer;
 
-class PhpLocAnalyzer
+use SebastianBergmann\PHPLOC\Analyser;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use TomasVotruba\ShopsysAnalysis\Finder\PhpFilesFinder;
+
+final class PhpLocAnalyzer
 {
+    /**
+     * @var SymfonyStyle
+     */
+    private $symfonyStyle;
 
+    /**
+     * @var PhpFilesFinder
+     */
+    private $phpFilesFinder;
+
+    public function __construct(SymfonyStyle $symfonyStyle, PhpFilesFinder $phpFilesFinder)
+    {
+        $this->symfonyStyle = $symfonyStyle;
+        $this->phpFilesFinder = $phpFilesFinder;
+    }
+
+    public function process(string $name, string $source): void
+    {
+        $count = $this->analyzeLocInDirectory($source);
+
+        $this->symfonyStyle->title($name);
+
+        $this->symfonyStyle->writeln(sprintf(
+            'Lines of code (LOC): %d',
+            $count['loc']
+        ));
+
+        $this->symfonyStyle->writeln(sprintf(
+            'Cyclomatic Complexity per Class: %0.2f',
+            $count['classCcnAvg']
+        ));
+
+        $this->symfonyStyle->writeln(sprintf(
+            'Cyclomatic Complexity per Method: %0.2f',
+            $count['methodCcnAvg']
+        ));
+
+        $this->symfonyStyle->writeln(sprintf(
+            'Cyclomatic Complexity per Method: %0.2f',
+            $count['methodCcnAvg']
+        ));
+
+        $this->symfonyStyle->writeln(sprintf(
+            'Number of Methods/Number of Classes: %0.2f',
+            $count['methods'] / $count['classes'] ?: 1
+        ));
+    }
+
+    /**
+     * @return mixed[]
+     */
+    private function analyzeLocInDirectory(string $directory): array
+    {
+        $files = $this->phpFilesFinder->findInDirectory($directory);
+
+        // have to be created fresh for every project, uses cache
+        return (new Analyser)->countFiles($files, false);
+    }
 }
--- src/Command/AnalyzeCommand.php
+++ src/Command/AnalyzeCommand.php
@@ -2,14 +2,11 @@
 
 namespace TomasVotruba\ShopsysAnalysis\Command;
 
-use SebastianBergmann\PHPLOC\Analyser;
-use SplFileInfo;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Finder\Finder;
-use TomasVotruba\ShopsysAnalysis\Finder\PhpFilesFinder;
+use TomasVotruba\ShopsysAnalysis\Analyzer\PhpLocAnalyzer;
 use TomasVotruba\ShopsysAnalysis\ProjectProvider;
 
 final class AnalyzeCommand extends Command
@@ -25,18 +22,18 @@ final class AnalyzeCommand extends Command
     private $projectProvider;
 
     /**
-     * @var PhpFilesFinder
+     * @var PhpLocAnalyzer
      */
-    private $phpFilesFinder;
+    private $phpLocAnalyzer;
 
     public function __construct(
         SymfonyStyle $symfonyStyle,
         ProjectProvider $projectProvider,
-        PhpFilesFinder $phpFilesFinder
+        PhpLocAnalyzer $phpLocAnalyzer
     ) {
         $this->symfonyStyle = $symfonyStyle;
         $this->projectProvider = $projectProvider;
-        $this->phpFilesFinder = $phpFilesFinder;
+        $this->phpLocAnalyzer = $phpLocAnalyzer;
         parent::__construct();
     }
 
@@ -48,49 +45,11 @@ final class AnalyzeCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         foreach ($this->projectProvider->provide() as $name => $source) {
-            $count = $this->analyzeLocInDirectory($source);
-
-            $this->symfonyStyle->title($name);
-
-            $this->symfonyStyle->writeln(sprintf(
-                'Lines of code (LOC): %d',
-                $count['loc']
-            ));
-
-            $this->symfonyStyle->writeln(sprintf(
-                'Cyclomatic Complexity per Class: %0.2f',
-                $count['classCcnAvg']
-            ));
-
-            $this->symfonyStyle->writeln(sprintf(
-                'Cyclomatic Complexity per Method: %0.2f',
-                $count['methodCcnAvg']
-            ));
-
-            $this->symfonyStyle->writeln(sprintf(
-                'Cyclomatic Complexity per Method: %0.2f',
-                $count['methodCcnAvg']
-            ));
-
-            $this->symfonyStyle->writeln(sprintf(
-                'Number of Methods/Number of Classes: %0.2f',
-                $count['methods'] / $count['classes'] ?: 1
-            ));
+            $this->phpLocAnalyzer->process($name, $source);
 
             $this->symfonyStyle->newLine(2);
         }
 
         return 1;
-    }
-
-    /**
-     * @return mixed[]
-     */
-    private function analyzeLocInDirectory(string $directory): array
-    {
-        $files = $this->phpFilesFinder->findInDirectory($directory);
-
-        // have to be created fresh for every project, uses cache
-        return (new Analyser)->countFiles($files, false);
     }
 }
