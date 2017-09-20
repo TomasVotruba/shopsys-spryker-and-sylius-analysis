e66e6e79e5516583432090b7941518292d68abbb
TomasVotruba <tomas.vot@gmail.com>
2017-09-20 16:57:53 +0200

    PHPStanCommand added

---
+++ src/Command/PHPStanCommand.php
@@ -0,0 +1,98 @@
+<?php declare(strict_types=1);
+
+namespace TomasVotruba\ShopsysAnalysis\Command;
+
+use Nette\Utils\Strings;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Process;
+use TomasVotruba\ShopsysAnalysis\Contract\Analyzer\AnalyzerInterface;
+use TomasVotruba\ShopsysAnalysis\PHPStanProjectProvider;
+use TomasVotruba\ShopsysAnalysis\ProjectProvider;
+
+final class PHPStanCommand extends Command
+{
+    /**
+     * @var int
+     */
+    private const FIRST_LEVEL = 0;
+
+    /**
+     * @var int
+     */
+    private const LEVEL_COUNT = 7;
+
+    /**
+     * @var string
+     */
+    private const ERROR_COUNT_PATTERN = '#Found (?<errorCount>[0-9]+) errors#';
+
+    /**
+     * @var string
+     */
+    public const NAME = 'phpstan';
+
+    /**
+     * @var SymfonyStyle
+     */
+    private $symfonyStyle;
+
+    /**
+     * @var PHPStanProjectProvider
+     */
+    private $phpStanProjectProvider;
+
+    public function __construct(SymfonyStyle $symfonyStyle, PHPStanProjectProvider $phpStanProjectProvider)
+    {
+        $this->symfonyStyle = $symfonyStyle;
+        $this->phpStanProjectProvider = $phpStanProjectProvider;
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setName(self::NAME);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        foreach ($this->phpStanProjectProvider->provide() as $name => $cli) {
+            $this->symfonyStyle->title($name);
+
+            for ($level = self::FIRST_LEVEL; $level <= self::LEVEL_COUNT; $level++) {
+                $finalCli = sprintf($cli, $level);
+                $tempFile = $this->createTempFileName($name, $level);
+
+                $process = new Process($finalCli . ' > ' . $tempFile);
+                $process->run();
+
+                $errorCount = $this->getErrorCountFromTempFile($tempFile);
+
+                $this->symfonyStyle->writeln(sprintf(
+                    'Level %d: %d errors',
+                    $level,
+                    $errorCount
+                ));
+            }
+
+            $this->symfonyStyle->newLine();
+        }
+
+        return 0;
+    }
+
+    private function createTempFileName(string $name, int $level): string
+    {
+        return 'temp/phpstan-' . strtolower($name) . '-level-' . $level;
+    }
+
+    private function getErrorCountFromTempFile(string $tempFile): int
+    {
+        $tempFileContent = file_get_contents($tempFile);
+        $matches = Strings::match($tempFileContent, self::ERROR_COUNT_PATTERN);
+
+        return (int) ($matches['errorCount'] ?? 0);
+    }
+}
---
+++ src/PHPStanProjectProvider.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace TomasVotruba\ShopsysAnalysis;
+
+final class PHPStanProjectProvider
+{
+    /**
+     * @return string[]
+     */
+    public function provide(): array
+    {
+        return [
+            'Shopsys' => 'vendor/bin/phpstan analyse project/shopsys/src project/shopsys/vendor/shopsys --configuration phpstan-shopsys.neon --level %d',
+            'Sylius' => 'vendor/bin/phpstan analyse project/spryker/vendor/spryker --configuration phpstan-spryker.neon --level %d',
+            'Spryker' => 'vendor/bin/phpstan analyse project/sylius/src --configuration phpstan-sylius.neon --level %d'
+        ];
+    }
+}
