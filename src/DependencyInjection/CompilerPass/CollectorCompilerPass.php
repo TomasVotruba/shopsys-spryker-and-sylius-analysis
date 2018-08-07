576f2192d77d5f0b411bcf43f102a14e96374d7e
Tomas Votruba <tomas.vot@gmail.com>
2018-08-07 14:16:25 +0200

    Use Command collector instead of ApplicationFactory

--- src/DependencyInjection/CompilerPass/CollectorCompilerPass.php
+++ src/DependencyInjection/CompilerPass/CollectorCompilerPass.php
@@ -2,26 +2,50 @@
 
 namespace TomasVotruba\ShopsysAnalysis\DependencyInjection\CompilerPass;
 
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symplify\PackageBuilder\Adapter\Symfony\DependencyInjection\DefinitionCollector;
+use Symplify\PackageBuilder\DependencyInjection\DefinitionCollector;
+use Symplify\PackageBuilder\DependencyInjection\DefinitionFinder;
 use TomasVotruba\ShopsysAnalysis\Command\AnalyzeCommand;
 use TomasVotruba\ShopsysAnalysis\Contract\Analyzer\AnalyzerInterface;
 
 final class CollectorCompilerPass implements CompilerPassInterface
 {
+    /**
+     * @var DefinitionCollector
+     */
+    private $definitionCollector;
+
+    public function __construct()
+    {
+        $this->definitionCollector = (new DefinitionCollector(new DefinitionFinder()));
+    }
+
     public function process(ContainerBuilder $containerBuilder): void
     {
         $this->collectAnalyzersToAnalyzeCommand($containerBuilder);
+        $this->collectCommandsToApplication($containerBuilder);
     }
 
     private function collectAnalyzersToAnalyzeCommand(ContainerBuilder $containerBuilder): void
     {
-        DefinitionCollector::loadCollectorWithType(
+        $this->definitionCollector->loadCollectorWithType(
             $containerBuilder,
             AnalyzeCommand::class,
             AnalyzerInterface::class,
             'addAnalyzer'
+        );
+    }
+
+    private function collectCommandsToApplication(ContainerBuilder $containerBuilder): void
+    {
+        $this->definitionCollector->loadCollectorWithType(
+            $containerBuilder,
+            Application::class,
+            Command::class,
+            'add'
         );
     }
 }
